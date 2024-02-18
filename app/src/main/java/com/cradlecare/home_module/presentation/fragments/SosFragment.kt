3c93deb9e39dcc70f75d6f45bd7c2f42b4f12249
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-02-19 00:57:11 +0530

    [Code Changes]: Added on click functionality on the sos fragment and now will add the text to the sos fragment tomorrow.

--- app/src/main/java/com/cradlecare/home_module/presentation/fragments/SosFragment.kt
+++ app/src/main/java/com/cradlecare/home_module/presentation/fragments/SosFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import com.cradlecare.R
+import androidx.lifecycle.lifecycleScope
+import com.cradlecare.databinding.FragmentSosBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SosFragment : Fragment() {
+    private lateinit var binding: FragmentSosBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +26,30 @@ class SosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sos, container, false)
+        binding = FragmentSosBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpUi()
+
+        setUpObserver()
+    }
+
+    private fun setUpObserver() {
+
+    }
+
+    private fun setUpUi() {
+        binding.innerCircle.setOnClickListener {
+            lifecycleScope.launch {
+                delay(1000)
+                binding.sosInactiveState.isVisible = false
+                binding.sosActiveState.isVisible = true
+            }
+        }
     }
 
 }
